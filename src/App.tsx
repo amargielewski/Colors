ee9c9ff1d4d80d5987bf8d92d975ff60b4bed76d
amargielewski <aleksander.margielewski4@gmail.com>
2022-03-09 01:50:16 +0100

    feat: scss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./App.scss";
+import { Input } from "components/Input/Input";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Input />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,7 @@
+import { HTMLInputTypeAttribute } from "react";
+import "./Input.scss";
+type InputProps = {};
+
+export const Input = (props: InputProps) => {
+  return <input className="input" />;
+};
